4128421a89e427a8bf4b84674c1f682189f6ec95
Anton Kaluzhin <kaluzhinas@gmail.com>
2023-05-29 00:31:32 +0700

    Delete comments

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pendulum
 
 dt = pendulum.now()
 
-a = dt.start_of('week')     # Monday
-b = dt.end_of('week')     # Monday
+a = dt.start_of('week')
+b = dt.end_of('week')
 
 print(a.to_datetime_string())
 print(b.to_datetime_string())
@@ -11,8 +11,8 @@ print(b.to_datetime_string())
 pendulum.week_starts_at(pendulum.FRIDAY)
 pendulum.week_ends_at(pendulum.THURSDAY)
 
-a = dt.start_of('week')     # Sunday
-b = dt.end_of('week')     # Thursday
+a = dt.start_of('week')
+b = dt.end_of('week')
 
 print('-----------------')
 
